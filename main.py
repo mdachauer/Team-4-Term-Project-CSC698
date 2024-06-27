eaacecbfd068636b44efeabab8049b18866fa0ba
mdachauer <mdachauer@smuhsd.org>
2024-06-27 16:32:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,21 @@
 #    - Have users press keys to submit answers seems simplest (A, B, C, D)
 
 # 2. Store answers (CSV)
-# Basic function:
 import csv
-with open('class_answers.csv', 'w', newline='') as csvfile:
-    file_writer = csv.writer(csvfile)
-    file_writer.writerow(['name', '1_answer', '2_answer', '3_answer'])
+def write_answer_file(filename, name, user_answers, class_answers):
+    class_answers.append([name] + user_answers)
+    with open(filename, 'w', newline='') as csvfile:
+        answer_writer = csv.writer(csvfile)
+        answer_writer.writerow(['name', '1_answer', '2_answer'])  # Add more depending on #of questions
+        answer_writer.writerows(class_answers)  #Writes in any existing answers
 
+def read_answer_file(filename):
+    answer_list = []
+    with open(filename, 'r', newline='') as csvfile:
+        answer_reader = csv.reader(csvfile)
+        for row in answer_reader:
+            answer_list.append(row)  #Creates a list we can accesss if needed
+            
 # 3. Compare them
 # 4. Display comparison to users
 
